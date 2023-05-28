d42836703758c32911701f068490d83603ac8d25
Your Name <mayasinalu@gmail.com>
2023-05-28 16:08:42 +0300

    Calculator Challenge

--- challenge.js
+++ challenge.js
@@ -1,30 +1,29 @@
 // Good Luck! You got this 💪🏾
+// Making a simple calculator using addition, subtraction, multiplication and division only.
 
 
-const calculator = (num1, num2, operator) => {
-    let result;
-
-    switch(operator){
-      case '+':
-        result = num1 + num2;
-        break;
-      case '-':
-        result = num1 - num2;
-        break;
-      case '*':
-        result = num1 * num2;
-        break;
-      case '/':
-        result = num1 / num2;
-        break;
-      default:
-        console.log('Invalid Operator');
-        return null;
+function calculate(num1, num2, operator) {
+    if (operator === "+") {
+      return num1 + num2;
+    } else if (operator === "-") {
+      return num1 - num2;
+    } else if (operator === "*") {
+      return num1 * num2;
+    } else if (operator === "/") {
+      if (num2 === 0) {
+        return "Cannot divide by zero";
+      } else {
+        return num1 / num2;
+      }
+    } else {
+      return "Invalid operator";
     }
+  }
 
-    console.log(`${num1} ${operator} ${num2} = ${result}`);
-    return result;
-  };
-
-  // Test cases
-  calculator(5, 2, '%')
+  // Example usage:
+  console.log(calculate(5, 5, "+")); // Output: 10
+  console.log(calculate(5, 5, "-")); // Output: 0
+  console.log(calculate(5, 5, "*")); // Output: 25
+  console.log(calculate(5, 5, "/")); // Output: 1
+  console.log(calculate(5, 0, "/")); // Output: "Cannot divide by zero"
+  console.log(calculate(5, 5, "%")); // Output: "Invalid operator"
